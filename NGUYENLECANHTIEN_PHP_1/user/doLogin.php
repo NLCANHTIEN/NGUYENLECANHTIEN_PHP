4b8cb6fc40a68f6bacde3058f3b7941d71831419
NLCANHTIEN <nguyenlecanhtien120602.work@gmail.com>
2024-03-29 14:42:20 +0700

    first commit

---
+++ NGUYENLECANHTIEN_PHP_1/user/doLogin.php
@@ -0,0 +1,24 @@
+<?php
+
+
+    $email = $_POST['email'];
+    $password = $_POST['pswd'];
+   
+    $sql = "SELECT * FROM user WHERE  email = '". $email."' AND password = '" . sha1($password) . "' ";
+    $result =  $f->getOne($sql);
+
+    if(!is_null($result)){
+        $_SESSION['user']=$result['username'];
+        $_SESSION['userId'] = $result['id'];
+        header("Location:index.php");
+       
+    }
+    else{
+    header("Location:http://localhost/nguyenlecanhtien/user/err404.php");
+    
+    echo "<button type='submit'>Login</button>";
+   
+    }
+    // return $result;
+
+    ?>
